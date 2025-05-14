871b821bfca07cdb05486c7fec70507fdbd12828
NikhilBhatt-15 <nikhilbhatt6980@gmail.com>
2025-05-14 22:40:26 +0530

    feat: add live-study-ruby.vercel.app to CORS allowed origins

--- server/app.js
+++ server/app.js
@@ -13,6 +13,7 @@ app.use(
             "http://localhost:8080",
             "https://localhost:8080",
             "*",
+            "https://live-study-ruby.vercel.app",
         ],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
